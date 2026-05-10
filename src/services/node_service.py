dd490b2c791f68cd6b5b9e6472a107a5f93f8bd2
paul-in <paul-in@mailo.com>
2026-05-10 14:54:05 +0200

    refactor: node_service cleaning

--- src/services/node_service.py
+++ src/services/node_service.py
@@ -1,34 +1,24 @@
 from typing import Optional, Union
 
 from src.core.node_scheduling_context import NodeSchedulingContext
-from src.db import Db
-from src.domain.domain_exceptions import InvalidUrl, NoNodeFound, NodeDeleted
-from src.models import type_data
+from src.domain.domain_exceptions import NoNodeFound, NodeDeleted
 from src.models.node import Node
 from src.models.node_data import NodeData
 from src.models.type_data import TypeData
 from src.repositories.node_repository import NodeRepository
-from src.services.node_format_service import NodeFormatService
-from src.services.priority_service import PriorityService
 from src.schemas.node_view import NodeView
 from src.schemas.node_metrics import NodeMetrics
 from src.schemas.node_update import NodeUpdate
 from src.models.node_content import NodeContent
-from src.services.ressource_service import RessourceService
 from src.types.node_type import NodeType
 from src.utils.time import overdue_ms, now_ms
-from src.utils.url import is_valid_url
 
 class NodeService:
     """
-    Node service logic (higher level than node_repository)
-
     Default behaviour is to filter nodes by aliveness (deleted_at field at None), excepted in get_node where we raise exception if node is deleted
     """
-    def __init__(self, db: Db, ressource_service: RessourceService, priority_service: PriorityService):
-        self._repo = NodeRepository(db)
-        self._ressource_service = ressource_service
-        self._priority_service = priority_service
+    def __init__(self, node_repository: NodeRepository):
+        self._repo = node_repository
         
     def get_node(self, node_id: int) -> Node:
         node = self._repo.get(node_id)
@@ -135,7 +125,7 @@ class NodeService:
             NodeSchedulingContext(
                 id=n.id,
                 type=n.type,
-                priority=n.priority, 
+                position=n.position, 
                 parent_id=n.parent_id,
                 due=n.due,
                 last_review=n.last_review,
@@ -144,6 +134,7 @@ class NodeService:
             )
             for n in nodes
         ]
+    
     def get_deleted_nodes_view(
         self,
         collection_id: int,
@@ -154,34 +145,13 @@ class NodeService:
             for i, n in enumerate(nodes)
         ]
 
-    def get_nodes_view(
-            self,
-            collection_id: int,
-            limit: int = 1000,
-    ) -> list[NodeView]:
-        nodes = self.get_nodes(collection_id, limit)
-        return [
-            self.node_to_view(n, i)
-            for i, n in enumerate(nodes)
-        ]
-    
-    def get_position(self, collection_id: int, node_id: int) -> int:
-        nodes = self.get_nodes(collection_id)
-
-        for i, node in enumerate(nodes):
-            if node.id == node_id:
-                return i
-
-        raise ValueError(f"Node {node_id} not found in collection {collection_id}")
-
-
-    def node_to_view(self, node: Node, position: int) -> NodeView:
+    def node_to_view(self, node: Node, priority: int) -> NodeView:
         return NodeView(
             id=node.id,
             collection_id=node.collection_id,
             type=node.type,
             content=node.content,
-            position=position,
+            priority=priority,
             parent_id=node.parent_id,
             due=node.due,
             data=node.data,
@@ -231,6 +201,9 @@ class NodeService:
         node_view = self.get_node(node_id)
         node_metrics = self.get_node_metrics(node_id)
         return {"view": node_view, "metrics": node_metrics}
+
+    def update_position(self, node_id: int, position: str):
+        self.update(node_id, NodeUpdate(position=position))
 
     def update(self, node_id: int, updates: NodeUpdate) -> Node:
         node = self.get_node(node_id)
