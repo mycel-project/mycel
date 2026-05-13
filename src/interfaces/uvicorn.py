67453827657d3eb2d41be649746193ebaea74c55
paul-in <paul-in@mailo.com>
2026-05-13 16:18:13 +0200

    feat: display network adress when launching Mycel

--- src/interfaces/uvicorn.py
+++ src/interfaces/uvicorn.py
@@ -1,5 +1,6 @@
 import uvicorn
 import asyncio
+import socket
 
 class UvicornServer:
     def __init__(self, host="0.0.0.0", port=8000):
@@ -13,10 +14,23 @@ class UvicornServer:
         config = uvicorn.Config(app, host=self.host, port=self.port, log_level="info", )
         self.server = uvicorn.Server(config)
         self.active = True
+        local_ip = self.get_local_ip()
+        print(f"Network: {local_ip}:{self.port}")
         await self.server.serve()
         self.active = False
         self.server = None
         self.task = None
+        
+    def get_local_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(("8.8.8.8", 80))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = "127.0.0.1"
+        finally:
+            s.close()
+        return ip
 
     async def start(self, app):
         if not self.active:
