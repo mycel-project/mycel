c208a18d169fbea90397ab9be05cfbce80ae2235
paul-in <paul-in@mailo.com>
2026-05-25 12:23:05 +0200

    fix: remove prerelease parsing from mycel (must be done in frontends)

--- src/core/app_infos.py
+++ src/core/app_infos.py
@@ -5,7 +5,6 @@ from pathlib import Path
 class AppInfos:
     def __init__(self):
         try:
-            raw = Path("VERSION").read_text().strip()
-            self.version = re.sub(r'-(alpha|beta|rc).*', '', raw)
+            self.version = Path("VERSION").read_text().strip()
         except FileNotFoundError:
             self.version = "unknown"
