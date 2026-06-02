cb100f9f83bb08c732cf55ce7954597a3c9c7d99
paul-in <paul-in@mailo.com>
2026-06-02 19:48:09 +0200

    tests: add tests for rest endpoints concerning users

--- tests/integration/interface/rest/test_rest.py
+++ tests/integration/interface/rest/test_rest.py
@@ -1,3 +1,23 @@
+class TestUser:
+    def test_create_user(self, api):
+        response = api.post("/users", body={"name": "test"})
+        assert response.status_code == 200
+        assert response.json()["data"]["name"] == "test"
+
+    def test_get_user_details(self, api, create_user):
+        user = create_user()
+        response = api.get(f"/users/{user.id}")
+        assert response.status_code == 200
+        data = response.json()
+        assert isinstance(data["data"]["created_at"], int)
+        assert data["data"]["id"] == user.id
+
+    def test_update_user(self, api, create_user):
+        user = create_user()
+        response = api.patch(f"/users/{user.id}", body={"name": "new name"})
+        assert response.status_code == 200
+        assert response.json()["data"]["name"] == "new name"
+
 class TestCollection:
     def test_list_collections(self, api, create_user, create_col):
         user = create_user()
@@ -26,3 +46,4 @@ class TestCollection:
         response = api.post("/collections", body={"name": "test"})    
         assert response.status_code == 200
         assert response.json()["data"]["name"] == "test"
+
