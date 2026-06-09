b1f77b2f6713c853ca2360094e4b48f91181a3bd
paul-in <paul-in@mailo.com>
2026-06-09 19:05:40 +0200

    chore(16): remove unused node metrics model

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,8 +1,7 @@
 from .node_view import NodeView
-from .node_metrics import NodeMetrics
 from .node_update import NodeUpdate
 from .collection_view import CollectionView
 from .algo_conf_update import AlgoConfUpdate
 from .collection_conf_update import CollectionConfUpdate
 
-__all__ = ["NodeView", "NodeUpdate", "NodeMetrics", "CollectionView", "CollectionConfUpdate",  "AlgoConfUpdate"]
+__all__ = ["NodeView", "NodeUpdate", "CollectionView", "CollectionConfUpdate",  "AlgoConfUpdate"]
